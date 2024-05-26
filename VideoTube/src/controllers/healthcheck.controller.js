c97c372d634746f2f6b1645451dec547ce1e5508
saran zafar <131322772+saranzafar@users.noreply.github.com>
2024-05-26 21:43:32 +0500

    created templet of all routes, models, and controllers

--- VideoTube/src/controllers/healthcheck.controller.js
+++ VideoTube/src/controllers/healthcheck.controller.js
@@ -1,3 +1,4 @@
+import { json } from "express"
 import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { AsyncHandler } from "../utils/asyncHandler.js"
@@ -5,6 +6,9 @@ import { AsyncHandler } from "../utils/asyncHandler.js"
 
 const healthcheck = AsyncHandler(async (req, res) => {
     //TODO: build a healthcheck response that simply returns the OK status as json with a message
+    return res.status(200, json(
+        new ApiResponse(200, "Everything is OK.", {})
+    ));
 })
 
 export {
